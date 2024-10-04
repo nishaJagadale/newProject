07bbc6714fb0b174105417190b6e7d5b866a9b83
nishaJagadale <nisha.jagadale@vpkbiet.org>
2024-10-04 15:18:57 +0530

    Update hello.java with cloud message

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ public class hello{
     public static void main(String args[]){
 System.out.println("Hello");
         System.out.println("world");
-         System.out.println("Welcome");
+         System.out.println("Welcome to the cloud");
     }
 }
